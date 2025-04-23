95ac9097f8fcdba5ee0f9892827a967f1bb6d2c8
IT Programmer <40290724+1IT-Programmer@users.noreply.github.com>
2025-04-23 12:08:06 +0500

    Create urls.py

---
+++ taxi/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
+    path('', views.home, name='home'),
+    path('core/', include('core.urls')),
+]
